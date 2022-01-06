84976b4b4607d76c12ee737ddf3310c6d51c5062
Sgoluc <lsgoliveira22@gmail.com>
2022-01-06 10:04:06 -0300

    First version of LeakCheck API query.

---
+++ leakCheck.py
@@ -0,0 +1,26 @@
+"""
+Sgoluc LeakCheck Script
+Only works with linked IPs!
+"""
+
+import sys
+import json
+import requests
+from pprint import pprint
+
+# Raw URL: https://leakcheck.net/api?key=YOUR_KEY&check=example@example.com&type=email
+# Usage: python leakCheck.py [TYPE] [SEARCH]
+
+api_file = open('leakcheck_api.txt', 'r')
+api_key = api_file.read()
+
+search_type = sys.argv[1]
+check = sys.argv[2]
+
+try:
+    request = requests.get('https://leakcheck.net/api?key=' + api_key + '&check=' + check + '&type=' + search_type)
+    response_json = json.loads(request.text)
+    pprint(response_json)
+
+except Exception as e:
+    print('Request not sent. Reason:', e)
