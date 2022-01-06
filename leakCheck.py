cb35d2ae4ea3c24fbc89b20c366e510fcc457460
Sgoluc <lsgoliveira22@gmail.com>
2022-01-06 14:03:20 -0300

    N/A

--- leakCheck.py
+++ leakCheck.py
@@ -20,7 +20,8 @@ check = sys.argv[2]
 try:
     request = requests.get('https://leakcheck.net/api?key=' + api_key + '&check=' + check + '&type=' + search_type)
     response = json.loads(request.text)
-    for i in response['result']:
-        pprint(i)
+#    for i in response['result']:
+#        pprint(i)
+    pprint(response['result'])
 except Exception as e:
     print('Request not sent. Reason:', e)
