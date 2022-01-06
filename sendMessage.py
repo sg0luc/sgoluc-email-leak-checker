2c9eda8fe3e75ab59311a0b0a9aa82fdcc3e6e5f
Sgoluc <lsgoliveira22@gmail.com>
2022-01-06 10:17:22 -0300

    First version of Telegram Simple SendMessage Function.

---
+++ sendMessage.py
@@ -0,0 +1,24 @@
+"""
+Sgoluc Simple Telegram
+SendMessage Function
+:)
+"""
+
+import requests
+
+def sendmessage(message):
+    token = '[YOUR_BOT_TOKEN]'
+    method = 'sendMessage'
+    print(token)
+    try:
+        requests.post(
+            url='https://api.telegram.org/bot{0}/{1}'.format(token, method),
+            data={'chat_id': '[CHANNEL_ID]', 'text': message}
+        ).json()
+        print("Message sent.")
+    except Exception as e:
+        print("Message not sent. Reason:", e)
+
+    return message
+
+sendmessage('[YOUR_MESSAGE')
