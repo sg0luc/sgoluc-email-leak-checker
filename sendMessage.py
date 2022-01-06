5d675d553bfe6abbc42d3520d7bd069beb76f5b6
Sgoluc <lsgoliveira22@gmail.com>
2022-01-06 14:00:20 -0300

    N/A

--- sendMessage.py
+++ sendMessage.py
@@ -8,13 +8,14 @@ import requests
 
 def sendmessage(message):
     tokenfile = open('C:\\Users\\lucas\\OneDrive\\Documents\\Projects\\Keys\\telegram_token.txt', 'r')
+    channelidfile = open('C:\\Users\\lucas\\OneDrive\\Documents\\Projects\\Keys\\telegram_channelid.txt', 'r')
     token = tokenfile.read()
+    channel_id = channelidfile.read()
     method = 'sendMessage'
-    print(token)
     try:
         requests.post(
             url='https://api.telegram.org/bot{0}/{1}'.format(token, method),
-            data={'chat_id': '[CHANNEL_ID]', 'text': message}
+            data={'chat_id': channel_id, 'text': message}
         ).json()
         print("Message sent.")
     except Exception as e:
