ede06392b931bbc4a0f97bb2a14d7702eb921292
sgoluc <lsgoliveira22@gmail.com>
2023-09-02 17:04:44 -0300

    Added README.md and modified leakCheck.py

--- leakCheck.py
+++ leakCheck.py
@@ -1,5 +1,5 @@
 """
-Sgoluc LeakCheck Script
+Sgoluc LeakCheck Test Script
 Only works with linked IPs!
 """
 
@@ -7,20 +7,25 @@ import sys
 import json
 import requests
 from pprint import pprint
+from sendMessage import sendmessage
 
-# Raw URL: https://leakcheck.net/api?key=YOUR_KEY&check=example@example.com&type=email
-# Usage: python leakCheck.py [TYPE] [SEARCH]
+# Raw URL: https://leakcheck.io/api?key=YOUR_KEY&check=example@example.com&type=email
+# Usage: python leakCheckTest.py [TYPE]
 
-api_file = open('C:\\Users\\lucas\\OneDrive\\Documents\\Projects\\Keys\\leakcheck_apikey.txt', 'r')
+api_file = open('C:\\Users\\lucas\\OneDrive\\Documents\\Projects\\Keys\\leakcheck_token.txt', 'r')
 api_key = api_file.read()
 
+emails_file = open('C:\\Users\\lucas\\OneDrive\\Documents\\Projects\\Keys\\emails.txt', 'r')
+emails_list = emails_file.readlines()
+
 search_type = sys.argv[1]
-check = sys.argv[2]
 
-try:
-    request = requests.get('https://leakcheck.net/api?key=' + api_key + '&check=' + check + '&type=' + search_type)
-    response_json = json.loads(request.text)
-    pprint(response_json)
-
-except Exception as e:
-    print('Request not sent. Reason: ', e)
+for e in emails_list:
+    try:
+        request = requests.get('https://leakcheck.io/api?key=' + api_key + '&type=' + search_type + '&check=' + e)
+        response = json.loads(request.text)
+        print('Results for', e, ':')
+        for i in response['result']:
+            pprint(i)
+    except Exception as e:
+        print('Request not sent. Reason: ', e)
