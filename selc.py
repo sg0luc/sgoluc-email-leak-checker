5c1ad07c262946a15c3542bd9a8050df3abd07a9
sgoluc <lsgoliveira22@gmail.com>
2023-09-03 09:10:19 -0300

    Major update on selc.py (old 'leakCheck.py')

---
+++ selc.py
@@ -0,0 +1,68 @@
+"""
+Sgoluc Email Leak Checker
+Author: Lucas "Sg0luc" Silveira
+Created: 2023-09-03
+Enjoy! <3
+"""
+import sys
+import json
+import requests
+from pprint import pprint
+from sendMessage import sendmessage
+
+# Raw URL: https://leakcheck.io/api?key=YOUR_KEY&check=example@example.com&type=email
+# Usage: python selc.py email
+
+api_file = open('C:\\Users\\lucas\\OneDrive\\Documents\\Projects\\Keys\\leakcheck_token.txt', 'r') # Store your LeakCheck token into this file and read it
+api_key = api_file.read()
+
+emails_file = open('C:\\Users\\lucas\\OneDrive\\Documents\\Projects\\Keys\\emails.txt', 'r') # Store your email list into this file
+emails_list = emails_file.readlines()
+
+search_type = sys.argv[1]
+
+sent_messages_file = 'C:\\Users\\lucas\\OneDrive\\Documents\\Projects\\Keys\\sent_messages.json' # Store already sent messages into 'sent_messages.json' file, to avoid realerting
+sent_messages = {}
+
+# Try to load 'sent_messages.json' file
+try: 
+    with open(sent_messages_file, 'r') as file:
+        sent_messages = json.load(file)
+except (FileNotFoundError, json.decoder.JSONDecodeError):
+    pass
+
+# Search for each email in 'emails.txt' file
+for e in emails_list:
+    try:
+        request = requests.get('https://leakcheck.io/api?key=' + api_key + '&type=' + search_type + '&check=' + e)
+        response = json.loads(request.text)
+
+        # Create initial message variable for each email and leak
+        for i in response['result']:
+            message = '🚨 Sgoluc Leak Check - Novo Vazamento Identificado 🚨' + '\n🔐 Credencial: ' + i['line'] + '\n'
+
+            # Verify if 'sources' exists and is not empty
+            if 'sources' in i and i['sources']:
+                sources_message = '🔍 Fonte(s): ' + ', '.join(i['sources']) + '\n'
+                message += sources_message
+
+            message += '🔥 Último vazamento: ' + i['last_breach']
+
+            # Create keys with email, source and last breach date to input into json file
+            keys = (e.strip(), ', '.join(i['sources']), i['last_breach'])
+
+            # Convert tuple into string to use it as key
+            keys_str = str(keys)
+
+            # Verify if message is already sent
+            if keys_str not in sent_messages:
+                sendmessage(message)
+                sent_messages[keys_str] = message
+
+    except Exception as e:
+        print('Request not sent. Reason:', e)
+
+# Save sent messages into sent_messages.json file
+with open(sent_messages_file, 'w') as savesentmessages:
+    json.dump(sent_messages, savesentmessages)
+
